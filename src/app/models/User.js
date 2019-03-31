7bb66abcd04b1175b1398960d61120768f64eec9
Ian Oliveira <Ian.Oliveira1@ibm.com@MacBook-Pro-de-Ian-2.local>
2019-03-31 18:51:30 -0300

    👨🏾‍🌾User Controller 👨🏾‍🌾

---
+++ src/app/models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../../config/auth");
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+UserSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcryptjs.hash(this.password, 8);
+});
+
+UserSchema.methods = {
+  compareHash(password) {
+    return bcryptjs.compare(password, this.password);
+  }
+};
+
+UserSchema.statics = {
+  generateToken({ id }) {
+    return jwt.sign({ id }, authConfig.secret, {
+      expiresIn: authConfig.ttl
+    });
+  }
+};
+
+module.exports = mongoose.model("User", UserSchema);
