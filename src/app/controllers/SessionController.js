5e3064a28161c65d63697f5831ded9a7b4e96af1
Ian Oliveira <Ian.Oliveira1@ibm.com@MacBook-Pro-de-Ian-2.local>
2019-03-31 18:50:56 -0300

    🔗 Session Controller 🔗

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,24 @@
+const User = require("../models/User");
+class SessionController {
+  async create(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "User not found" });
+      }
+
+      if (!(await user.compareHash(password))) {
+        return res.status(400).json({ error: "Invalid password" });
+      }
+
+      return res.json({ token: User.generateToken(user) });
+    } catch (error) {
+      return res.status(500).json({ error: "Something  went wrong" });
+    }
+  }
+
+  async delete(req, res) {}
+}
+
+module.exports = new SessionController();
