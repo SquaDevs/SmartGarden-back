1c180831c6fb244df5b97474be78e5946a4d47a0
Ian Oliveira <Ian.Oliveira1@ibm.com@MacBook-Pro-de-Ian-2.local>
2019-04-01 01:09:32 -0300

    🗺 plant data model 🗺

---
+++ src/app/models/PlantData.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate");
+
+const PlantData = mongoose.Schema({
+  plant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Plant"
+  },
+  temperature: {
+    type: Number
+  },
+  airHumidity: {
+    type: Number
+  },
+  groundHumidity: {
+    type: Number
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+PlantData.plugin(mongoosePaginate);
+
+module.exports = mongoose.model("PlantData", PlantData);
