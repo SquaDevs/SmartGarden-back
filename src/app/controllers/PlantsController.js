8c7603147f9373330e0d8c84baa51fd48ede1915
Ian Oliveira <Ian.Oliveira1@ibm.com@MacBook-Pro-de-Ian-2.local>
2019-03-31 23:06:18 -0300

    🌿Plant Controller🌿

---
+++ src/app/controllers/PlantsController.js
@@ -0,0 +1,60 @@
+const Plant = require("../models/Plant");
+class PlantsController {
+  async create(req, res) {
+    let plantToCreate = { ...req.body };
+    plantToCreate.location.type = "Point";
+    const plant = await Plant.create({ ...plantToCreate, owner: req.userId });
+    return res.status(201).json({ plant });
+  }
+  async update(req, res) {
+    const { title, description, location } = req.body;
+
+    if (!(title || description || location)) {
+      return res.status(406).json({ error: "No data to be updated" });
+    }
+
+    const plant = await Plant.findById(req.params.id);
+
+    const plantWdata = { ...plant["_doc"] };
+
+    plantWdata.title = title || plantWdata.title;
+    plantWdata.description = description || plantWdata.description;
+    plantWdata.location.coordinates =
+      location.coordinates || plantWdata.location.coordinates;
+
+    // delete plantWdata._id;
+    const plantUpdated = await Plant.findOneAndUpdate(
+      req.params.id,
+      plantWdata,
+      { new: true }
+    );
+
+    return res.json({ plant: plantUpdated });
+  }
+  async index(req, res) {
+    const filters = {};
+    if (req.query.title) {
+      filters.title = new RegExp(req.query.title, "i");
+    }
+
+    const plants = await Plant.paginate(filters, {
+      limit: 20,
+      page: req.query.page || 1,
+      sort: "createdAt",
+      populate: ["owner.name"]
+    });
+
+    return res.json({ plants });
+  }
+
+  async show(req, res) {
+    const plant = await Plant.findById(req.params.id);
+    return res.json({ plant });
+  }
+  async delete(req, res) {
+    await Plant.findByIdAndDelete(req.params.id);
+    return res.send();
+  }
+}
+
+module.exports = new PlantsController();
