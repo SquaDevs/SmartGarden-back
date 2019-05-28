2839faa6551a1b7011ed9eed673873634ef61dd6
Ian Gabriel Oliveira de Sousa <ian.oliv@outlook.com>
2019-05-28 16:06:27 -0300

    Setting https server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,22 @@
+const https = require("https");
+const http = require("http");
+const { readFileSync } = require("fs");
 const app = require("./app");
 
-app.listen(process.env.PORT || 3000);
+var key = readFileSync(__dirname + "/../../client-key.key");
+var cert = readFileSync(__dirname + "/../../cclient-cert.crt");
+var options = {
+  key: key,
+  cert: cert
+};
+
+let { PORT, HTTPS, NODE_ENV } = process.env;
+
+NODE_ENV &&
+  https.createServer(options, app).listen(HTTPS || 3000, () => {
+    console.log(`https Server is up  ${HTTPS || 3000}`);
+  });
+
+http.createServer(app).listen(PORT || 8080, () => {
+  console.log(`http  Server is up  on the port ${PORT || 8080}`);
+});
