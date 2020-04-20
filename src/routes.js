25e842cd20b1eb0ae27861f22b0154f7be5afa24
Ian Oliveira <ian.oliv@outlook.com>
2020-04-20 20:01:42 -0300

    Servers  instances

--- src/routes.js
+++ src/routes.js
@@ -1,66 +1,75 @@
-const routes = require("express").Router();
+const routes = require('express').Router()
 /**
  * Controllers
  */
 
-const UserController = require("./app/controllers/UserController");
-const SessionController = require("./app/controllers/SessionController");
-const PlantsController = require("./app/controllers/PlantsController");
-const PlantDataController = require("./app/controllers/PlantDataController");
+const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
+const PlantsController = require('./app/controllers/PlantsController')
+const PlantDataController = require('./app/controllers/PlantDataController')
+const InsightsController = require('./app/controllers/InsightsController')
 
 /**
  * Middlewares
  */
 
-const auth = require("./app/middleware/auth");
+const auth = require('./app/middleware/auth')
+// const brute = require('express-brute');
+// const bruteRedis = require('express-brute-redis');
 
 /**
  * Error Handler
  */
-const handler = require("express-async-handler");
+const handler = require('express-async-handler')
 
-routes.post("/user", handler(UserController.create));
-routes.post("/auth", handler(SessionController.create));
+routes.post('/user', handler(UserController.create))
+routes.post('/auth', handler(SessionController.create))
+routes.route('/plant/data/:token').post(handler(PlantDataController.create))
 
-routes.use(auth);
+routes.use(auth)
 
 /**
  * User Routes
  */
 
 routes
-  .route("/user")
+  .route('/user')
   .get(handler(UserController.show))
-  .put(handler(UserController.update));
+  .put(handler(UserController.update))
+  .delete(handler(UserController.delete))
 
 /**
  * Plant Routes
  */
 
 routes
-  .route("/plant")
+  .route('/plant')
   .post(handler(PlantsController.create))
-  .get(handler(PlantsController.index));
+  .get(handler(PlantsController.index))
 
 routes
-  .route("/plant/:id")
+  .route('/plant/:id')
   .get(handler(PlantsController.show))
   .put(handler(PlantsController.update))
-  .delete(handler(PlantsController.delete));
+  .delete(handler(PlantsController.delete))
+
+routes
+  .route('/insights/plant')
+  .post(handler(InsightsController.tgetPlanInsights))
 
 /**
  * Plant Data
  */
 
-//Create Plant data
-routes.route("/plant/data/:token").post(handler(PlantDataController.create));
-//view all plant data
-routes.route("/plant/:idPlant/data").get(handler(PlantDataController.index));
-//view a specific plant data
-routes.route("/plant/data/:idData").get(handler(PlantDataController.show));
-//view all plant data
-routes.route("/plants/data").get(handler(PlantDataController.index));
+// Create Plant data
+
+// view all plant data
+routes.route('/plant/:idPlant/data').get(handler(PlantDataController.index))
+// view a specific plant data
+routes.route('/plant/data/:idData').get(handler(PlantDataController.show))
+// view all plant data
+routes.route('/plants/data').get(handler(PlantDataController.index))
 // .put(handler(PlantDataController.update))->Cannot Update Plant Data
 // .delete(handler(PlantDataController.delete))->Cannot delete Plant Data
 
-module.exports = routes;
+module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,21 +1,28 @@
-const https = require("https");
-const http = require("http");
-const { readFileSync } = require("fs");
-const app = require("./app");
+const https = require('https')
+const http = require('http')
+const { join } = require('path')
+const { readFileSync } = require('fs')
+const app = require('./app')
 
-let { PORT, HTTPS } = process.env;
-if (HTTPS) {
-  var key = readFileSync(__dirname + "/../../client-key.pem");
-  var cert = readFileSync(__dirname + "/../../client-cert.pem");
-  var options = {
-    key: key,
-    cert: cert
-  };
+const appHTTP = http.createServer(app)
+// const io = require('socket.io')(appHTTP)
+// const ws = require('./ws')
+// const io = require('socket.io')(appHTTP);
 
-  https.createServer(options, app).listen(HTTPS || 3000, () => {
-    console.log(`https Server is up  ${HTTPS || 3000}`);
-  });
-}
-http.createServer(app).listen(PORT || 8080, () => {
-  console.log(`http  Server is up  on the port ${PORT || 8080}`);
-});
+const { PORT, HTTPS } = process.env
+
+HTTPS && https
+  .createServer({
+    key: readFileSync(join(__dirname, '/../../client-key.pem')),
+    cert: readFileSync(join(__dirname, '/../../client-cert.pem'))
+  }, app)
+  .listen(HTTPS, () => {
+    console.log(`https Server is up  ${ HTTPS }`)
+  })
+
+PORT && appHTTP
+  .listen(PORT, () => {
+    console.log(`http  Server is up  on the port ${ PORT }`)
+  })
+
+// ws(io)
