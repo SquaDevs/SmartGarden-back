1c1c5e8b020b0babb11a43ea21d417bb467ebfd2
Ian Oliveira <ian.oliv@outlook.com>
2020-04-20 19:48:38 -0300

    Versioning the API endpoint

--- src/app.js
+++ src/app.js
@@ -1,36 +1,38 @@
-require("dotenv").config({
-  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
-});
-var morgan = require("morgan");
-const express = require("express");
-const mongoose = require("mongoose");
-const validate = require("express-validation");
-const { uri } = require("./config/database");
-const cors = require("cors");
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+})
+var morgan = require('morgan')
+const express = require('express')
+const mongoose = require('mongoose')
+const validate = require('express-validation')
+const { uri } = require('./config/database')
+const cors = require('cors')
 class AppController {
   constructor() {
-    this.express = express();
-    this.middlewares();
-    this.routes();
-    this.database();
-    this.exception();
+    this.express = express()
+    this.middlewares()
+    this.routes()
+    this.database()
+    this.exception()
   }
+
   database() {
-    mongoose.connect(uri, { useCreateIndex: true, useNewUrlParser: true });
+    mongoose.connect(uri, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
   }
 
   middlewares() {
-    this.express.use(cors());
-    this.express.use(express.json());
-    this.express.use(morgan());
+    this.express.use(cors())
+    this.express.use(express.json())
+    this.express.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
     this.express.use((req, res, next) => {
-      console.log(req.headers);
-      console.log(req.body);
-      return next();
-    });
+      // console.log(req.headers);
+      // console.log(req.body);
+      return next()
+    })
   }
+
   routes() {
-    this.express.use(require("./routes"));
+    this.express.use('/v1', require('./routes'))
   }
 
   exception() {
@@ -38,12 +40,12 @@ class AppController {
       // if (err instanceof validate.ValidationError) {
       //   return res.status(err.status).json(err);
       // }
-      console.log(err);
+      console.log(err)
       return res
         .status(err.status || 500)
-        .json({ error: "Internarl Server Error" });
-    });
+        .json({ error: 'Internarl Server Error' })
+    })
   }
 }
 
-module.exports = new AppController().express;
+module.exports = new AppController().express
